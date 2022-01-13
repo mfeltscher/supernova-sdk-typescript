6ffc79fbe454b2b1da80b78634b43aed0937f760
Jiri Trecak <jiri@supernova.io>
2022-01-13 15:21:54 +0100

    #package - Make SDK typescript imports available

--- src/exports.ts
+++ src/exports.ts
@@ -1,2 +1,117 @@
 // Export main Supernova object
 export { Supernova } from '../src/core/SDKSupernova'
+
+// Export auxiliary objects
+export { Brand } from '../src/core/SDKBrand'
+export { DesignSystem } from '../src/core/SDKDesignSystem'
+export { DesignSystemVersion } from '../src/core/SDKDesignSystemVersion'
+export { Workspace } from '../src/core/SDKWorkspace'
+
+// Assets
+export { Asset } from '../src/model/assets/SDKAsset'
+export { RenderedAsset } from '../src/model/assets/SDKRenderedAsset'
+
+// Component
+export { Component } from '../src/model/components/SDKComponent'
+
+// Documentation / Core
+export { Documentation } from '../src/core/SDKDocumentation'
+
+// Documentation / Blocks
+export { DocumentationPageBlockAsset } from '../src/model/documentation/blocks/SDKDocumentationPageBlockAsset'
+export { DocumentationPageBlockAssets } from '../src/model/documentation/blocks/SDKDocumentationPageBlockAssets'
+export { DocumentationPageBlockCallout } from '../src/model/documentation/blocks/SDKDocumentationPageBlockCallout'
+export { DocumentationPageBlockCode } from '../src/model/documentation/blocks/SDKDocumentationPageBlockCode'
+export { DocumentationPageBlockCustom } from '../src/model/documentation/blocks/SDKDocumentationPageBlockCustom'
+export { DocumentationPageBlockDivider } from '../src/model/documentation/blocks/SDKDocumentationPageBlockDivider'
+export { DocumentationPageBlockEmbedFigma } from '../src/model/documentation/blocks/SDKDocumentationPageBlockEmbedFigma'
+export { DocumentationPageBlockEmbedGeneric } from '../src/model/documentation/blocks/SDKDocumentationPageBlockEmbedGeneric'
+export { DocumentationPageBlockEmbedLink } from '../src/model/documentation/blocks/SDKDocumentationPageBlockEmbedLink'
+export { DocumentationPageBlockEmbedStorybook } from '../src/model/documentation/blocks/SDKDocumentationPageBlockEmbedStorybook'
+export { DocumentationPageBlockEmbedYoutube } from '../src/model/documentation/blocks/SDKDocumentationPageBlockEmbedYoutube'
+export { DocumentationPageBlockFrame } from '../src/model/documentation/blocks/SDKDocumentationPageBlockFrame'
+export { DocumentationPageBlockFrames } from '../src/model/documentation/blocks/SDKDocumentationPageBlockFrames'
+export { DocumentationPageBlockHeading } from '../src/model/documentation/blocks/SDKDocumentationPageBlockHeading'
+export { DocumentationPageBlockImage } from '../src/model/documentation/blocks/SDKDocumentationPageBlockImage'
+export { DocumentationPageOrderedList } from '../src/model/documentation/blocks/SDKDocumentationPageBlockOrderedList'
+export { DocumentationPageBlockQuote } from '../src/model/documentation/blocks/SDKDocumentationPageBlockQuote'
+export { DocumentationPageBlockRenderCode } from '../src/model/documentation/blocks/SDKDocumentationPageBlockRenderCode'
+export { DocumentationPageBlockShortcut } from '../src/model/documentation/blocks/SDKDocumentationPageBlockShortcut'
+export { DocumentationPageBlockShortcuts } from '../src/model/documentation/blocks/SDKDocumentationPageBlockShortcuts'
+export { DocumentationPageBlockText } from '../src/model/documentation/blocks/SDKDocumentationPageBlockText'
+export { DocumentationPageBlockToken } from '../src/model/documentation/blocks/SDKDocumentationPageBlockToken'
+export { DocumentationPageBlockTokenGroup } from '../src/model/documentation/blocks/SDKDocumentationPageBlockTokenGroup'
+export { DocumentationPageBlockTokenList } from '../src/model/documentation/blocks/SDKDocumentationPageBlockTokenList'
+export { DocumentationPageUnorderedList } from '../src/model/documentation/blocks/SDKDocumentationPageBlockUnorderedList'
+
+// Documentation / Custom Blocks
+export { DocumentationCustomBlock } from '../src/model/documentation/custom_blocks/SDKDocumentationCustomBlock'
+export { DocumentationCustomBlockProperty } from '../src/model/documentation/custom_blocks/SDKDocumentationCustomBlockProperty'
+
+// Documentation / Item Partials
+export { DocumentationItemHeader } from '../src/model/documentation/configuration/SDKDocumentationItemHeader'
+export { DocumentationItemConfiguration } from '../src/model/documentation/configuration/SDKDocumentationItemConfiguration'
+
+
+// Documentation / Main
+export { DocumentationConfiguration } from '../src/model/documentation/SDKDocumentationConfiguration'
+export { DocumentationGroup } from '../src/model/documentation/SDKDocumentationGroup'
+export { DocumentationItem } from '../src/model/documentation/SDKDocumentationItem'
+export { DocumentationPage } from '../src/model/documentation/SDKDocumentationPage'
+export { DocumentationPageBlock } from '../src/model/documentation/SDKDocumentationPageBlock'
+export { DocumentationRichText } from '../src/model/documentation/SDKDocumentationRichText'
+export { RichTextSpan } from '../src/model/documentation/SDKDocumentationRichTextSpan'
+export { RichTextSpanAttribute } from '../src/model/documentation/SDKDocumentationRichTextSpanAttribute'
+
+// Groups
+export { AssetGroup } from '../src/model/groups/SDKAssetGroup'
+export { ComponentGroup } from '../src/model/groups/SDKComponentGroup'
+export { TokenGroup } from '../src/model/groups/SDKTokenGroup'
+
+// Enums
+export { Alignment } from '../src/model/enums/SDKAlignment'
+export { AssetFormat } from '../src/model/enums/SDKAssetFormat'
+export { AssetScale } from '../src/model/enums/SDKAssetScale'
+export { AssetScaleType } from '../src/model/enums/SDKAssetScaleType'
+export { BlurType } from '../src/model/enums/SDKBlurType'
+export { BorderPosition } from '../src/model/enums/SDKBorderPosition'
+export { CustomTokenPropertyType } from '../src/model/enums/SDKCustomTokenPropertyType'
+export { DocumentationCalloutType } from '../src/model/enums/SDKDocumentationCalloutType'
+export { DocumentationGroupBehavior } from '../src/model/enums/SDKDocumentationGroupBehavior'
+export { DocumentationHeadingType } from '../src/model/enums/SDKDocumentationHeadingType'
+export { DocumentationItemType } from '../src/model/enums/SDKDocumentationItemType'
+export { DocumentationPageBlockType } from '../src/model/enums/SDKDocumentationPageBlockType'
+export { FrameAlignment } from '../src/model/enums/SDKFrameAlignment'
+export { FrameLayout } from '../src/model/enums/SDKFrameLayout'
+export { GradientType } from '../src/model/enums/SDKGradientType'
+export { RichTextSpanAttributeType } from '../src/model/enums/SDKRichTextSpanAttributeType'
+export { ShadowType } from '../src/model/enums/SDKShadowType'
+export { SourceType } from '../src/model/enums/SDKSourceType'
+export { TextCase } from '../src/model/enums/SDKTextCase'
+export { TextDecoration } from '../src/model/enums/SDKTextDecoration'
+export { TokenType } from '../src/model/enums/SDKTokenType'
+export { Unit } from '../src/model/enums/SDKUnit'
+
+// Supporting objects
+export { ComponentOrigin } from '../src/model/support/SDKComponentOrigin'
+export { File } from '../src/model/support/SDKFile'
+export { Size } from '../src/model/support/SDKSize'
+export { TokenOrigin } from '../src/model/support/SDKTokenOrigin'
+
+// Tokens
+export { BlurToken } from '../src/model/tokens/SDKBlurToken'
+export { BorderToken } from '../src/model/tokens/SDKBorderToken'
+export { ColorToken } from '../src/model/tokens/SDKColorToken'
+export { FontToken } from '../src/model/tokens/SDKFontToken'
+export { GenericToken } from '../src/model/tokens/SDKGenericToken'
+export { GradientToken } from '../src/model/tokens/SDKGradientToken'
+export { MeasureToken } from '../src/model/tokens/SDKMeasureToken'
+export { RadiusToken } from '../src/model/tokens/SDKRadiusToken'
+export { ShadowToken } from '../src/model/tokens/SDKShadowToken'
+export { TypographyToken } from '../src/model/tokens/SDKTypographyToken'
+export { TextToken } from '../src/model/tokens/SDKTextToken'
+export { Token } from '../src/model/tokens/SDKToken'
+export { TokenProperty } from '../src/model/tokens/SDKTokenProperty'
+export { TokenValue, ColorTokenValue, BlurTokenValue, BorderTokenValue, FontTokenValue, GenericTokenValue, GradientTokenValue, MeasureTokenValue, RadiusTokenValue, ShadowTokenValue, TypographyTokenValue, TextTokenValue } from '../src/model/tokens/SDKTokenValue'
+
+export { CustomTokenProperty } from '../src/model/tokens/configuration/SDKCustomTokenProperty'
